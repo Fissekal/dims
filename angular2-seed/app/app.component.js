367ad4a49039332b649a4932448923f20f483f41
jannik <shyeinua@gmail.com>
2016-09-08 08:38:01 +0200

    Added router function

--- angular2-seed/app/app.component.js
+++ angular2-seed/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './subscribe-form.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './albums.component', './contact.component', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,25 +10,36 @@ System.register(['angular2/core', './subscribe-form.component'], function(export
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, subscribe_form_component_1;
+    var core_1, albums_component_1, contact_component_1, router_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (subscribe_form_component_1_1) {
-                subscribe_form_component_1 = subscribe_form_component_1_1;
+            function (albums_component_1_1) {
+                albums_component_1 = albums_component_1_1;
+            },
+            function (contact_component_1_1) {
+                contact_component_1 = contact_component_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
                 }
                 AppComponent = __decorate([
+                    router_1.RouteConfig([
+                        { path: '/albums', name: 'Albums', component: albums_component_1.AlbumsComponent, useAsDefault: true },
+                        { path: '/contact', name: 'Contact', component: contact_component_1.ContactComponent },
+                        { path: '/*other', name: 'Other', redirectTo: ['Albums'] }
+                    ]),
                     core_1.Component({
                         selector: 'my-app',
-                        directives: [subscribe_form_component_1.SubscribeFormComponent],
-                        template: "\n    <subscribe-form></subscribe-form>\n    "
+                        directives: [router_1.RouterOutlet],
+                        templateUrl: 'app/app.component.html'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,12 +1,19 @@
 import {Component} from 'angular2/core';
 import {SubscribeFormComponent} from './subscribe-form.component'
+import {AlbumsComponent} from './albums.component';
+import {ContactComponent} from './contact.component';
+import {RouteConfig, RouterOutlet} from 'angular2/router';
+
+@RouteConfig([
+    { path: '/albums', name: 'Albums', component: AlbumsComponent, useAsDefault: true},
+    { path: '/contact', name: 'Contact', component: ContactComponent},
+    { path: '/*other', name: 'Other', redirectTo: ['Albums'] }
+])
 
 @Component( {
     selector: 'my-app',
-    directives: [SubscribeFormComponent],
-    template: `
-    <subscribe-form></subscribe-form>
-    `
+    directives: [RouterOutlet],
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
     
